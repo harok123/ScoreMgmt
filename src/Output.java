1ac95e0f59a0ead1ab4f7f34c715d848ae2123a1
harok123 <gkfhrwkd23@naver.com>
2022-08-05 17:06:04 +0900

    add project output

--- src/Output.java
+++ src/Output.java
@@ -7,7 +7,7 @@ public class Output {
 		}
 	}
 	
-	private void pl() {
+	public void pl() {
 		System.out.println("<<<<센텀대학교 성적관리프로그램>>>>");
 		System.out.println("학번\t이름\t국어\t영어\t수학\t전산\t총점\t평균\t평점");
 		System.out.println("---------------------------------------------");
